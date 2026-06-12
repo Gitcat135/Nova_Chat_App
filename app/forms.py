b2a7bbedece43d8de7c213577ea9f561a7ccd291
Gitcat135 <prince32cat@gmail.com>
2026-06-12 18:47:33 +0300

    Class 4

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,8 @@ from wtforms.validators import DataRequired, length, Email, EqualTo
 
 class SignInForm(FlaskForm):
     """Sign in form"""
-    Email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
-class RegisterForm(FlaskForm):
-    """Register form"""
     
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import SignInForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -10,9 +11,11 @@ def index():
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
     """Sign In URL"""
-    return render_template('sign_in.html', title='Sign In Page')
+    form = SignInForm()
+    return render_template('sign_in.html', title='Sign In Page', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Register URL"""
+    
     return render_template('register.html', title='Register Page')
