f1f84e353a94767966e91da5bcd7ce6e5198de53
Gitcat135 <prince32cat@gmail.com>
2026-06-27 16:40:32 +0300

    Class 7

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from app.models import User
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     """Index URL"""
-    return render_template('index.html', title='Home Page')
+    return render_template('index.html', title='Welcome')
 
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
@@ -20,10 +20,10 @@ def sign_in():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid email or password. Please try again.')  # Flash an error message for
-            return render_template('sign_in.html', title='Sign In Page', form=form)
+            return render_template('sign_in.html', title='Sign In', form=form)
         login_user(user, remember=form.remember_me.data)  # Log in the user
         flash(f'Welcome, {user.username}! You have successfully signed in.')  # Flash a success message
-        return redirect(url_for('index'))  # Redirect to index page after successful login
+        return redirect(url_for('feed'))  # Redirect to Feed page after successful login
         
     return render_template('sign_in.html', title='Sign In Page', form=form)
 
@@ -39,11 +39,24 @@ def register():
         db.session.commit()  # Commit the session to save the user to the database
         flash(f'Congratulations {form.username.data}, you are now a registered user!')  # Flash a success message
         return redirect(url_for('sign_in'))  # Redirect to sign in page after successful registration
-    return render_template('register.html', title='Register Page', form=form)
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/logout')
 def logout():
-    """Logout URL"""
-    logout_user()  # Log out the current user
-    flash('You have been logged out.')  # Flash a message indicating successful logout
-    return redirect(url_for('login'))  # Redirect to index page after logout
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/feed')
+def feed():
+    """Feed URL"""
+    return render_template('feed.html', title='Feed')
+
+@app.route('/profile')
+def profile():
+    """Profile URL"""
+    return render_template('profile.html', title='Profile')
+
+@app.route('/edit_profile')
+def edit_profile():
+    """Edit Proflie URL"""
+    return render_template('edit_profile.html', title='Edit Profile')
